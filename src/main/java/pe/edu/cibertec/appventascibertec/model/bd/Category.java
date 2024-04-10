5400b65d36d45ab836b31d74846987e5c9246ab1
Luis <i202211476@cibertec.edu.pe>
2024-04-09 20:38:17 -0500

    Primera parte del mapeo de objetos BD

--- src/main/java/pe/edu/cibertec/appventascibertec/model/bd/Category.java
+++ src/main/java/pe/edu/cibertec/appventascibertec/model/bd/Category.java
@@ -1,15 +1,16 @@
 package pe.edu.cibertec.appventascibertec.model.bd;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
 @Entity
 @Table(name = "categories")
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "CategoryID")
+
     private Integer categoryid;
     @Column(name = "CategoryName")
     private String categoryname;
--- src/main/java/pe/edu/cibertec/appventascibertec/model/bd/Customers.java
+++ src/main/java/pe/edu/cibertec/appventascibertec/model/bd/Customers.java
@@ -1,4 +1,36 @@
 package pe.edu.cibertec.appventascibertec.model.bd;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="customers")
 public class Customers {
+    @Id
+    @Column(name="CustomerID")
+    private String customerID;
+    @Column(name="CompanyName")
+    private String companyname;
+    @Column(name="ContactName")
+    private String contactname;
+    @Column(name="ContactTitle")
+    private String contacttitle;
+    @Column(name="Address")
+    private String address;
+    @Column(name="City")
+    private String city;
+    @Column(name="Region")
+    private String region;
+    @Column(name="PostalCode")
+    private String postalcode;
+    @Column(name="Country")
+    private String country;
+    @Column(name="Phone")
+    private String phone;
+    @Column(name="Fax")
+    private String fax;
 }
